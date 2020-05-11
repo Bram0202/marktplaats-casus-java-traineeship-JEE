70dcbb1f8430812afd4165e0cec58bb62f5bfbc3
Bram <brambakx@pm.me>
2020-05-11 09:54:03 +0200

    Aanmelden veranderd in registeren

--- src/main/java/frontend/pagina/StartPagina.java
+++ src/main/java/frontend/pagina/StartPagina.java
@@ -5,7 +5,7 @@ public class StartPagina extends AbstractPagina {
     public StartPagina() {
         setPaginaTitel("startpagina");
         setMenuOptie1("Inloggen");
-        setMenuOptie2("Aanmelden");
+        setMenuOptie2("Registreren");
         setMenuOptie3("Wachtwoord vergeten");
         setMenuOptie4("");
         setMenuOptie1Link(new LoginPagina());
